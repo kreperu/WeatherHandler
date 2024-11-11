3d5b7a223d41b9782aa02a666892bede72e656fa
kreperu <thekreperu@gmail.com>
2024-11-11 01:06:04 +0100

    Include License

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,20 @@
+/*mapapogoda.pl Weather Service Handler Startpoint
+    Copyright (C) 2024  Jakub "kreperu" Sowula
+    kripkrep@gmail.com
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.*/
+
 import ovh.foxtaillab.weather_handler.*;
 
 import java.util.concurrent.ScheduledExecutorService;
@@ -6,6 +23,10 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Weather Handler  Copyright (C) 2024  Jakub \"kreperu\" Sowula (kripkrep@gmail.com)\n" +
+                "    This program comes with ABSOLUTELY NO WARRANTY;\n" +
+                "    This is free software, and you are welcome to redistribute it\n" +
+                "    under certain conditions;");
         ScheduledExecutorService ses = new ScheduledThreadPoolExecutor(1);
         ses.scheduleAtFixedRate(new DataUpdateTask(), 0, Util.DNL_INTERVAL, TimeUnit.SECONDS);
         DataServer ds = new DataServer(Util.PORT, Util.MAX_CONNECTIONS, Util.PATH);
--- src/ovh/foxtaillab/weather_handler/DataUpdateTask.java
+++ src/ovh/foxtaillab/weather_handler/DataUpdateTask.java
@@ -1,16 +1,29 @@
+/*IMGW API request task
+    Copyright (C) 2024  Jakub "kreperu" Sowula
+    kripkrep@gmail.com
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.*/
+
 package ovh.foxtaillab.weather_handler;
 
 import java.net.*;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.sql.Time;
 import java.time.Duration;
-import java.time.Instant;
 import java.time.LocalDateTime;
-import java.util.HashMap;
 import java.util.Map;
 
 public class DataUpdateTask implements Runnable {
